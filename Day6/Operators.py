31a353559e9f0498ab90a6463b104670c36dd253
sairammerugumalla1 <sairammerugumalla@gmail.com>
2026-02-04 16:05:04 +0530

    update all the files till Day7

---
+++ Day6/Operators.py
@@ -0,0 +1,80 @@
+'''
+Docstring for Day6.Operators
+
+operators are symbols used to perform some operations between
+variables , values 
+
+types of operators 
+
+#Arithmetic
++ , - ,* ,/(division) ,// (floor division) ,% (modulas),**(power)
+
+# // foor division ?? always give quotient 
+# / division --- always gives the quotient and gives only float value as output 
+# % modulus --- always gives the remainder as an output 
+
+
+
+# comparison 
+compares two variables or values 
+== , != , >,< , <= ,>=
+
+
+# Assignment
+=, += ,-=,*=, /=
+
+# Logical
+ AND 
+if both the conditions are true then it returns true
+ 
+ OR 
+ if atleast one conditions is true then it returns true
+
+ NOT
+  returns true if false
+  returns false if true  
+
+
+# identity 
+is --- returns true if it finds the value 
+
+is not ---- returns true  if it doesn't find the value 
+
+
+
+lst=[10, 20, 30 ,40]
+is = 20 ?? 
+
+# bit wise
+5+3
+
+
+# membership 
+ in ---- returns true if the value is present in  the sequence 
+ not ----returns true , if the value is not present 
+
+
+for i in range():
+
+'''
+print(10/5) # 2.0 (Q)
+print()
+print(10//5)# 2 (Q)
+print()
+print(10%5)# 0 (R)
+
+print(10**3)
+
+a=5
+#a=a+1 #0+1=1
+a+=1
+print(a)
+a*=2 #a=a*2
+print(a)
+
+b=12
+
+# legal age 21 
+# leagl for voting 18 
+# AND
+
