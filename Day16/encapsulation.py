7c71cd48f43437e3abc12833542039502f426f9d
sairammerugumalla9966 <sairammerugumalla@gmail.com>
2026-03-01 16:11:29 +0530

    updated files till Day19

--- Day16/encapsulation.py
+++ Day16/encapsulation.py
@@ -1,5 +1,5 @@
 # Encapsulation : binding data and methods together and restrict direct access 
-
+'''
 class bankAccount:
 
     def __init__(self):
@@ -22,17 +22,44 @@ print(b.balance)
 # public 
 # private 
 # protected 
-
+'''
 class bankAccount:
 
     def __init__(self):
         self.__balance = 5000
 
+    def deposit(self,amount):
+        if amount > 0:
+            self.__balance += amount
+    
+    def withdraw(self, amount):
+        if amount <= self.__balance :
+            self.__balance -= amount
+
     def display(self):
         print(self.__balance)
 
 b=bankAccount()
-b.display()
+b.deposit(2000)
+b.display() # 7000
+b.withdraw(1000)
+b.display() # 6000
+
+# Abstraction  : hiding implemention details and only show essintial details 
 
 
+from abc import ABC , abstractmethod
+
+class payment(ABC) : 
+
+    @abstractmethod
+    def pay(self , amount):
+        pass
+
+class UPI(payment):
+    def pay(self , amount):
+        print("payment is successful", amount)
+
+obj=UPI()
+obj.pay(500)
 
---
+++ Day18/virtual_environment.py
@@ -0,0 +1,13 @@
+# virtual environment 
+# it is a directory which contains installations and packages of that particular python version 
+
+# how to create virtual environment ???
+
+# python -m venv nameofvirtualenv
+
+# source sai_venv/scripts/activate     or    sai_venv\scripts\activate 
+
+# deactivate 
+
+# pip install application_name
+# pip list ---->>> list of applications or packages installed in venv
